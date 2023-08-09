593a28d76be83f1c89a265d28b849a9d3b569f0d
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2023-08-09 16:16:56 -0400

    Added module docstring

--- desktop_icons_fix.py
+++ desktop_icons_fix.py
@@ -1,3 +1,23 @@
+"""
+desktop_icons_fix.py
+
+This script is used to fix issues related to desktop icons on Windows. It performs the following tasks:
+
+1. Moves shortcuts from the public desktop to the user's desktop.
+2. Deletes cache database files associated with desktop icons.
+3. Restarts the Windows Explorer process to apply changes.
+
+Usage:
+    - Simply run the application to automatically fix your desktop icons.
+
+Note:
+    - The script was designed for Windows 10, but it should also work on Windows 11.
+
+Disclaimer:
+    - Use this script at your own risk. It performs file operations and system process restarts, which could potentially cause unintended consequences.
+
+"""
+
 import os
 import shutil
 
